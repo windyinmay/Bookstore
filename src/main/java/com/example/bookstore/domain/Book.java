791dd1aff9b9b23f592142324826a976a47edc0b
Quỳnh Anh <quynhanhmay@Quynhs-MacBook-Pro.local>
2021-02-08 18:35:08 +0200

    init bookstore

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,48 @@
+package com.example.bookstore.domain;
+
+public class Book {
+	private String title;
+	private int year;
+	private String author;
+	private String isbn;
+	private double price;
+	
+	public Book(String title, String author, int year, String isbn, double price) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package com.example.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class BookController {
+	@RequestMapping("index")
+	public String index() {
+		return "index";
+	}
+}
